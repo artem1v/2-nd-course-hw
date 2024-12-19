45102130e3f8be9227255f1d49cad8c95a7b276a
John Doe <johndoe@email.com>
2024-12-19 18:32:34 +0500

    4 homework assignment

--- script6.js
+++ script6.js
@@ -18,4 +18,68 @@
  }
 */
 
+/*
+Задание 2
+Дан массив: 
+[1, 5, 4, 10, 0, 3].
+
+Найдите индекс значения 
+4 в этом массиве.
+*/
+/*
+const numbers = [1, 5, 4, 10, 0, 3];
+
+numbers.forEach((el, index) => {
+    console.log(`${index}: ${el}`);
+});
+*/
+/*
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] == 4) break;
+    console.log(numbers[i]);
+}
+*/
+
+/*
+Задание 3
+Дан массив чисел: 
+[1, 3, 5, 10, 20]
+.
+
+С помощью метода 
+join
+ выведите элементы массива через пробел (пустую строку ' ').
+*/
+
+/*
+let number = [1, 3, 5, 10, 20];
+number = number.join(' ');
+console.log(number);
+*/
+
+/*
+Задание 4
+С помощью вложенных циклов создайте многомерный массив вида: 
+[[1, 1, 1], [1, 1, 1], [1, 1, 1]].
+*/
+
+
+const number = [
+    [1, 1, 1,],
+    [1, 1, 1,],
+    [1, 1, 1,],
+];
+
+for (const arrIn of number) {
+    for (const element of arrIn) {
+        console.log(arrIn);
+    }
+}
+
+
+for (const arrIn of number) {
+        console.log(arrIn);
+    }
+
+
 
